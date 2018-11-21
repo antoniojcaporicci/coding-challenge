15976679ae0c1ebeea29416cea7c3f1e9d2bb5cd
Antonio Caporicci <antonioc@ecreations.net>
2018-11-21 10:13:55 -0600

    implemented required functionality

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+	has_many :comments, dependent: :destroy
+
+	def self.search(search)
+		where("title LIKE ?", "%#{search}%") 
+		where("body LIKE ?", "%#{search}%")
+  end
 end
